b99daf98f8ff832d4cb857abec4f59ea11733361
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-07-27 13:34:18 -0700

    structure for commands

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # bot.py
 import os
-import random
+
 from dotenv import load_dotenv
-import requests
-import json
+
+
 
 
 import discord
@@ -16,13 +16,29 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 async def on_ready():
 
 
-# Begin the process of making a profile for the user. Definitely need language(s). Maybe a self-intro? 
+
 @bot.command(name='profile', help='Starts the process of making a profile')
 async def make_profile(ctx):
+    """
+    Starts the process of making a profile for the user. Guide users on specifc info needed and maybe a self intro.
+    Profile info should be something that stays the same. We could set up a questionaire for the user and they answer by reacting. 
+    Stuff like language, region, rank, 
+
+    Args:
+        ctx: context of the command. Usually the channel 
+    """
     await ctx.send("Making profile")
+
 
 @bot.command(name='find_team')
 async def find_team(ctx):
+    """
+    Uses profile and situation-specific info to find a team. Info we need includes 
+
+
+    Args:
+        ctx: context of the command. Usually the channel 
+    """
     await ctx.send("Finding you a team. Hang tight!")
 
 
