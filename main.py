7dba21e1a474ab0f5b8a80f579288bb714bca753
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-07-27 13:43:59 -0700

    fixed compilation issues and runs bot

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
+    pass
 
 
 
@@ -45,4 +46,6 @@ async def find_team(ctx):
     """
     await ctx.send("Finding you a team. Hang tight!")
 
+
+bot.run(TOKEN)
 
